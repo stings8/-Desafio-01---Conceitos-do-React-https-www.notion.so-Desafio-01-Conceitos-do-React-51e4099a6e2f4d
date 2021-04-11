ea932876860e004af2fd64958ee6e13bcda20f29
Insight-lab <insight-lab@Mac-mini-de-Insight-lab.local>
2021-04-11 16:05:12 -0300

    taskList completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,15 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!!newTaskTitle) {
+      setTasks(prevState => [...prevState, 
+        {
+          id: Math.random(),
+          title: newTaskTitle,
+          isComplete: false
+        }
+      ])
+      setNewTaskTitle('')
+    }
+   
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksUpdated =  tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : {...task});
+    setTasks(tasksUpdated);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
+
+  console.log(tasks)
 
   return (
     <section className="task-list container">
